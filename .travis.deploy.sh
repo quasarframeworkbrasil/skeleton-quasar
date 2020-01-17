56b9ae776ef4a14a138fb206761a7b4276ce4060
William Correa <wilcorrea@gmail.com>
2020-01-17 02:01:24 -0300

    [feature] Setup travis and gh pages

--- .travis.deploy.sh
+++ .travis.deploy.sh
@@ -1,6 +1,32 @@
 #!/usr/bin/env bash
 
 cd ${TRAVIS_BUILD_DIR}
+
 cp .env.js.stage .env.js
 quasar build
-cp -R dist/spa/* docs
+
+rm -rf node_modules/
+rm -rf .quasar/
+rm -rf .bin/
+rm -rf src/
+
+rm -rf .env
+rm -rf .eslintignore
+rm -rf .eslintrc.js
+rm -rf .gitignore
+rm -rf .postcssrc.js
+rm -rf .stylintrc
+rm -rf .travis.deploy.sh
+rm -rf .travis.yml
+rm -rf babel.config.js
+rm -rf command.sh
+rm -rf docker-compose.yml
+rm -rf package.json
+rm -rf quasar.conf.js
+rm -rf README.md
+rm -rf version.ejs
+rm -rf yarn.lock
+
+cp -R dist/spa/* ./
+
+rm -rf dist/
