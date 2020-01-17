893597b2c7f87d31ec7b6dfd604baf30b317b367
William Correa <wilcorrea@gmail.com>
2020-01-17 00:54:26 -0300

    [feature] Generate fake data

--- src/domains/Admin/Profile/Schema/ProfileService.js
+++ src/domains/Admin/Profile/Schema/ProfileService.js
@@ -1,5 +1,6 @@
 import Rest from 'src/app/Services/Rest'
 import { resource } from '../settings'
+import { promisify, uuid, get } from 'src/app/Util/general'
 
 /**
  * @type {ProfileService}
@@ -9,4 +10,88 @@ export default class ProfileService extends Rest {
    * @type {String}
    */
   resource = resource
+
+  /**
+   * @param {Object} record
+   * @returns {Promise}
+   */
+  create (record) {
+    return promisify({ ...record, id: uuid() })
+  }
+
+  /**
+   * @param {Object} record
+   * @returns {Promise}
+   */
+  update (record) {
+    return promisify({ ...record })
+  }
+
+  /**
+   * @param {string | number | Record<string, any>} record
+   * @param {boolean} trash
+   * @returns {Promise}
+   */
+  read (record, trash = false) {
+    return promisify({
+      data: {
+        id: uuid(),
+        name: 'Profile fake',
+        reference: 'path/' + Math.ceil(Math.random() * 100),
+        actions: []
+      }
+    })
+  }
+
+  /**
+   * @param {Object} record
+   * @returns {Promise}
+   */
+  destroy (record) {
+    return promisify({ ...record })
+  }
+
+  /**
+   * @param {Record<string, string | number>} parameters
+   * @param {Array<string>} [filters] = []
+   * @param {boolean} [trash] = false
+   * @returns {Promise<any>}
+   */
+  paginate (parameters, filters, trash = false) {
+    const { pagination } = parameters
+
+    const rowsPerPage = get(pagination, 'rowsPerPage', this.size)
+    const sortBy = get(pagination, 'sortBy')
+    const descending = get(pagination, 'descending')
+    const page = get(pagination, 'page', 1)
+
+    const rowsNumber = 25
+    const pagesNumber = Math.ceil(rowsNumber / rowsPerPage)
+    let length = rowsPerPage
+    if (page === pagesNumber) {
+      length = rowsNumber % (pagesNumber - 1)
+    } else if (page > pagesNumber) {
+      length = 0
+    }
+
+    const generator = (value, index) => {
+      const counter = (page - 1) * rowsPerPage + index + 1
+      return {
+        id: uuid(),
+        name: `Profile fake ${counter}`,
+        reference: 'path/' + Math.ceil(Math.random() * 100),
+        actions: []
+      }
+    }
+
+    return promisify({
+      rowsPerPage: rowsPerPage,
+      rowsNumber: rowsNumber,
+      pagesNumber: pagesNumber,
+      sortBy: sortBy,
+      descending: descending,
+      page: page,
+      rows: Array.from({ length }, generator)
+    })
+  }
 }
--- src/domains/Admin/User/settings.js
+++ src/domains/Admin/User/settings.js
@@ -3,3 +3,6 @@ export const path = '/dashboard/admin/user'
 
 /** @type {string} */
 export const domain = 'admin.user'
+
+/** @type {string} */
+export const resource = '/admin/user'
