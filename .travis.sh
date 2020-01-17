8b9d5c8d031311257a64d439036732aa89f8d502
William Correa <wilcorrea@gmail.com>
2020-01-17 02:22:50 -0300

    [feature] Setup travis and gh pages

--- .travis.sh
+++ .travis.sh
@@ -2,9 +2,10 @@
 
 cd ${TRAVIS_BUILD_DIR}
 
-if [[ -f .env.js.stage ]]; then
+if [ -f .env.js.stage ]; then
   cp .env.js.stage .env.js
 fi
+
 docker run -v "${PWD}":/app -w /app tevun/quasar:latest \
   quasar build
   cp -R dist/spa/* ./ &&\
