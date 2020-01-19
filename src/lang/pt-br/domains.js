cde6ad50662062bdbad4515512a2eb6f48f66379
William Correa <wilcorrea@gmail.com>
2020-01-19 10:24:28 -0300

    [feature] Refactor some issues

--- src/lang/pt-br/domains.js
+++ src/lang/pt-br/domains.js
@@ -2,8 +2,6 @@
 import action from 'src/domains/Admin/Action/pt-br'
 import profile from 'src/domains/Admin/Profile/pt-br'
 import user from 'src/domains/Admin/User/pt-br'
-// domains/Example
-import movie from 'src/domains/Example/Movie/pt-br'
 // domains/Help
 import home from 'src/domains/Home/pt-br'
 // domains/Report
@@ -15,9 +13,6 @@ export default {
   home,
   admin: {
     action, profile, user
-  },
-  example: {
-    movie
   },
   report
 }
