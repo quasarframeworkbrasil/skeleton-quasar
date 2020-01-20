9a09177f7900f50a339be9827020947f814a7b36
William Correa <wilcorrea@gmail.com>
2020-01-20 10:33:21 -0300

    [example] Create the views of Category

---
+++ src/views/dashboard/general/category/CategoryForm.vue
@@ -0,0 +1,28 @@
+<template>
+  <SchemaForm v-bind="bind" />
+</template>
+
+<script type="text/javascript">
+import View from 'src/app/Agnostic/Adapters/View'
+import Schema from 'src/domains/General/Category/Schema/CategorySchema'
+
+/**
+ */
+export default {
+  /**
+   */
+  extends: View,
+  /**
+   */
+  name: 'CategoryForm',
+  /**
+   */
+  schema: Schema
+}
+</script>
+
+<style
+  lang="stylus"
+  rel="stylesheet/stylus"
+>
+</style>
---
+++ src/views/dashboard/general/category/CategoryTable.vue
@@ -0,0 +1,28 @@
+<template>
+  <SchemaTable v-bind="bind" />
+</template>
+
+<script type="text/javascript">
+import View from 'src/app/Agnostic/Adapters/View'
+import Schema from 'src/domains/General/Category/Schema/CategorySchema'
+
+/**
+ */
+export default {
+  /**
+   */
+  extends: View,
+  /**
+   */
+  name: 'CategoryTable',
+  /**
+   */
+  schema: Schema
+}
+</script>
+
+<style
+  lang="stylus"
+  rel="stylesheet/stylus"
+>
+</style>
