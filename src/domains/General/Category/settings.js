d43f731a0f270cc6300d2d4a4e8267a7623afde6
William Correa <wilcorrea@gmail.com>
2020-01-20 10:32:29 -0300

    [example] Create the settings for Category

---
+++ src/domains/General/Category/settings.js
@@ -0,0 +1,14 @@
+/**
+ * @type {string}
+ */
+export const path = '/dashboard/general/category'
+
+/**
+ * @type {string}
+ */
+export const domain = 'general.category'
+
+/**
+ * @type {string}
+ */
+export const resource = '/general/category'
