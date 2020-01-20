a1416f69ca8398708ef87ba88b48a74ccc4a5f70
William Correa <wilcorrea@gmail.com>
2020-01-20 00:21:12 -0300

    [feature] Refactor some issues

--- src/app/Components/Schema/Form/Mixins/SchemaFormFieldError.js
+++ src/app/Components/Schema/Form/Mixins/SchemaFormFieldError.js
@@ -1,4 +1,5 @@
 import SchemaError from 'src/app/Components/Schema/Form/SchemaFormError'
+import { replacement } from 'src/app/Util/string'
 
 /**
  * @mixin {SchemaFormFieldError}
@@ -26,20 +27,12 @@ export default {
      */
     errorContent (key) {
       const errorMessages = []
-      const forEach = (validation) => {
-        if (!validations[validation]) {
-          let paths = [
-            `domains.${this.domain}.validations.${validation}`.replace(/\//g, '.'),
-            `validation.${validation}`
-          ]
-          // validations.$params[validation]
-          errorMessages.push(this.$lang(paths))
-        }
-      }
-
       const validations = this.$util.get(this.validations, `record.${key}`)
       if (validations) {
-        Object.keys(validations.$params).forEach(forEach)
+        const $params = Object.keys(validations.$params)
+        $params.forEach((validation) => {
+          this.errorValidation(validations, validation, errorMessages)
+        })
       }
       if (this.errors[key]) {
         const paths = [
@@ -50,6 +43,28 @@ export default {
         errorMessages.push(this.$lang(paths) || this.errors[key])
       }
       return errorMessages.join(' / ')
+    },
+    /**
+     * @param {Object} validations
+     * @param {string} rule
+     * @param {Array} errorMessages
+     */
+    errorValidation (validations, rule, errorMessages) {
+      // it is ok, no worries
+      if (validations[rule]) {
+        return
+      }
+      // create paths to i18n
+      const paths = [
+        `domains.${this.domain}.validations.${rule}`.replace(/\//g, '.'),
+        `validation.${rule}`
+      ]
+      // get the i18n message
+      const template = this.$lang(paths) || ''
+      // prepare replaces to apply to message
+      const replaces = validations.$params[rule] || {}
+      // execute replacement to apply the parameters to message
+      errorMessages.push(replacement(template, replaces))
     },
     /**
      * @param {Function} h
