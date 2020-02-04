24ec376e3ee453699ad4de6433ae41fc904f641f
William Correa <wilcorrea@gmail.com>
2020-02-03 23:35:58 -0300

    [feature] Add i18n to master detail (embed) actions

--- src/lang/pt-br/agnostic.js
+++ src/lang/pt-br/agnostic.js
@@ -1,11 +1,11 @@
 import { primaryKey } from 'src/settings/schema'
+import actions from 'src/lang/pt-br/actions'
 
 export default {
   /**
    * @type {Object}
    */
-  scope: {
-  },
+  scope: {},
   table: {
     search: 'Pesquisar...',
     columns: 'Colunas'
@@ -33,6 +33,9 @@ export default {
       cancel: 'Cancelar',
       clear: 'Limpar Seleção',
       search: 'Pesquisar'
+    },
+    image: {
+      button: 'Selecione uma imagem'
     }
   },
   dialog: {
@@ -57,87 +60,22 @@ export default {
     }
   },
   actions: {
-    create: {
-      label: 'Salvar',
-      success: 'Registro criado com sucesso',
-      fail: 'Opa, não foi possível realizar a operação',
-      validation: 'Verifique os campos destacados',
-      tooltip: 'Cria um novo registro'
-    },
-    update: {
-      label: 'Salvar',
-      success: 'Registro atualizado com sucesso',
-      fail: 'Opa, não foi possível realizar a operação',
-      validation: 'Verifique os campos destacados',
-      tooltip: 'Atualiza um registro'
-    },
-    reset: {
-      label: 'Limpar',
-      tooltip: ''
-    },
-    add: {
-      label: 'Novo',
-      tooltip: 'Abre a tela de criação de um novo registro'
-    },
-    trash: {
-      label: 'Lixeira',
-      tooltip: 'Abre a lixeira para a recuperação de um registro'
-    },
-    edit: {
-      label: 'Editar',
-      tooManySelected: 'Esta operação não pode ser feita para vários registros',
-      noItems: 'Esta operação precisa de um item selecionado ou um contexto',
-      tooltip: 'Abre a tela de alteração de um registro'
-    },
-    destroy: {
-      label: 'Apagar',
-      success: 'Registro(s) removido(s) com sucesso',
-      confirm: 'Deseja apagar este(s) registro(s)?',
-      noItems: 'Esta operação precisa de um item selecionado ou um contexto',
-      fail: 'Whoops, não foi possível realizar a operação',
-      tooltip: 'Remove um registro'
-    },
-    restore: {
-      label: 'Restaurar',
-      success: 'Registro(s) restaurado(s) com sucesso',
-      confirm: 'Deseja restaurar este(s) registro(s)?',
-      noItems: 'Esta operação precisa de um item selecionado ou um contexto',
-      fail: 'Whoops, não foi possível realizar a operação',
-      tooltip: 'Restaura um registro que estava na lixeira'
-    },
-    view: {
-      label: 'Visualizar',
-      tooManySelected: 'Esta operação não pode ser feita para vários registros',
-      noItems: 'Esta operação precisa de um item selecionado ou um contexto',
-      tooltip: 'Abre a tela de visualização de um registro'
-    },
-    home: {
-      label: 'Listagem',
-      tooltip: 'Volta para a lista principal'
-    },
-    back: {
-      label: 'Voltar',
-      tooltip: 'Volta para a tela anterior'
-    },
-    print: {
-      label: 'Impressão',
-      tooltip: 'Visualiza a impressão do conteúdo desta página'
-    },
-    refresh: {
-      label: 'Atualizar',
-      tooltip: 'Atualiza os registros carregados na tela'
-    },
-    'sort-clear': {
-      noSort: 'A ordenação não está ativa para ser reiniciada',
-      tooltip: 'Remove a ordenação aplicada à tela'
-    },
-    search: {
-      label: 'Pesquisar',
-      tooltip: 'Pesquisa pelos valores informados no formulário'
-    },
-    'search-clear': {
-      label: 'Limpar',
-      tooltip: 'Limpa todos os filtros aplicados'
-    }
+    ...actions,
+    'md-create': actions.create,
+    'md-update': actions.update,
+    'md-reset': actions.reset,
+    'md-add': actions.add,
+    'md-trash': actions.trash,
+    'md-edit': actions.edit,
+    'md-destroy': actions.destroy,
+    'md-restore': actions.restore,
+    'md-view': actions.view,
+    'md-home': actions.home,
+    'md-back': actions.back,
+    'md-print': actions.print,
+    'md-refresh': actions.refresh,
+    'md-sort-clear': actions['sort-clear'],
+    'md-search': actions.search,
+    'md-search-clear': actions['search-clear']
   }
 }
